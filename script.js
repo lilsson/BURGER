88c95ced86f8fce2d1e807a0ead14f996988479e
lilsson <pirckhelavalile@gmail.com>
2021-11-10 18:27:45 +0300

    add input

--- script.js
+++ script.js
@@ -4,3 +4,45 @@ let navbarLinks = document.getElementById('navbarLinks');
 toggleburger.addEventListener('click', function(){
     navbarLinks.classList.toggle('active');
 })
+
+// input
+
+let button = document.querySelector('.button');
+let input = document.querySelector('.input-form');
+let ul = document.querySelector('.input-ul');
+let form = document.querySelector('.form-wraper');
+
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // 1. get input value
+    let value = input.value;
+
+    // 2.add input value in ul 
+    if (value != ' ') {
+        let li = document.createElement('li');
+
+        let btnDelete = document.createElement('button');
+        btnDelete.textContent = 'Delete';
+        btnDelete.addEventListener('click', function() {
+            li.remove();
+        })
+        
+    
+        li.textContent = value;
+    
+        li.appendChild(btnDelete);
+        ul.appendChild(li);
+
+         // 3.clear input
+        input.value = ' ';
+    }
+   
+});
+
+
+document.querySelector('.clearlist').addEventListener('click', function() {
+    ul.innerHTML = ' ';
+})
